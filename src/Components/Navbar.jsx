d8a6bce7bb46134d1c54e07d256b29400eaae747
DanKhan1 <chaudriali034@gmail.com>
2024-12-27 12:50:28 +0500

    this is my fourth change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,17 @@
 import React, { useContext } from "react";
 import { ProductContex } from "../Utils/Contex";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   const [products] = useContext(ProductContex);
-  const distinct_category = products && products.reduce((acc,currentval)=>[...acc,currentval.category],[])
-  console.log("specific category",distinct_category);
+  let distinct_category = products && products.reduce((acc,currentval)=>[...acc,currentval.category],[])
+  // console.log("specific category",distinct_category);
+  distinct_category = [...new Set(distinct_category)]
+  console.log("ALL different category",distinct_category);
   
+  const color = () =>{
+    return `rgba(${(Math.random()*255).toFixed()},${(Math.random()*255).toFixed()},${(Math.random()*255).toFixed()},0.5)`
+  }
+
   return (
     <>
       <nav className="w-[15%] h-full bg-zinc-100 flex flex-col items-center pt-5">
@@ -17,24 +24,14 @@ const Navbar = () => {
         </a>
         <hr className="w-[80%] my-3" />
         <h2 className="text-xl mb-3 w-[80%]">Category</h2>
-        <ul className=" w-[80%]">
-          <li className="mb-3 flex items-center">
-            <span className="rounded-full mr-2 w-[15px] h-[15px] bg-blue-400"></span>
-            Category 1
-          </li>
-          <li className="mb-3 flex items-center">
-            <span className="rounded-full mr-2 w-[15px] h-[15px] bg-blue-400"></span>
-            Category 2
-          </li>
-          <li className="mb-3 flex items-center">
-            <span className="rounded-full mr-2 w-[15px] h-[15px] bg-blue-400"></span>
-            Category 3
-          </li>
-          <li className="mb-3 flex items-center">
-            <span className="rounded-full mr-2 w-[15px] h-[15px] bg-blue-400"></span>
-            Category 4
-          </li>
-        </ul>
+        <div className=" w-[80%]">
+          {distinct_category.map((categoryitem,index)=>(
+                <Link key={index} to={`/?category=${categoryitem}`} className="mb-3 flex items-center">
+                <span style={{backgroundColor: color()}} className="rounded-full mr-2 w-[15px] h-[15px]"></span>
+                {categoryitem}
+              </Link>
+          ))}
+        </div>
       </nav>
     </>
   );
